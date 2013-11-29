a5653f90e70291e1e100143f8fc343902a369a3f
Eugenio Piasini <e.piasini@ucl.ac.uk>
2013-11-29 14:16:35 +0000

    Encapsulate simulation with Poisson stims in a simple function.

--- neuroConstruct/lemsSimulations/integrationWindow/integrationWindow.py
+++ neuroConstruct/lemsSimulations/integrationWindow/integrationWindow.py
@@ -1,63 +1,36 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
+import sys
 import subprocess
 import numpy as np
 from matplotlib import pyplot as plt
 import xml.etree.ElementTree as ET
+
 script_dir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.dirname(script_dir))
 
-def create_stim_rate_file(exc_rate, delay=1000, stim_type='regular'):
-    Lems = ET.Element("Lems")
-    # include Inputs.xml
-    include = ET.SubElement(Lems, "Include")
-    include.set("file", "Inputs.xml")
-    if stim_type=='regular':
-        exc_spikegen = ET.SubElement(Lems, "spikeGeneratorDelay")
-        exc_spikegen.set("period", "{} ms".format(1000./exc_rate))
-        exc_spikegen.set("delay", "{} ms".format(delay))
-    elif stim_type=='poisson':
-        exc_spikegen = ET.SubElement(Lems, "spikeGeneratorPoisson")
-        exc_spikegen.set("averageRate", "{} Hz".format(exc_rate))
-    exc_spikegen.set("id", "mossySpiker")
-    # save to disk
-    with open(script_dir+"/inputRate.xml", "w") as freq_file:
-        ET.ElementTree(Lems).write(freq_file)
-
-def moving_causal_average(signal, window_length, timestep):
-    window_size = int(round(window_length/timestep))
-    window = np.hstack((np.ones(window_size), np.zeros(window_size)))/float(window_size)
-    return np.convolve(signal, window, 'same')
+import simulation_tools
 
 def main():
-    exc_rate_range = np.array([40])
-    sim_duration = 50. # s
+    exc_rate_range = np.array([50])
+    out_rates = np.zeros_like(exc_rate_range)
+    sim_duration = 60. # s
     n_bins = 200
-    upper_hist_limit = 0.300
-
-    out_filename = "voltage.dat"
+    upper_hist_limit = 0.300 # s
 
     for k, exc_rate in enumerate(exc_rate_range):
-        create_stim_rate_file(exc_rate, stim_type='poisson')
-        if os.path.isfile(out_filename):
-            os.remove(out_filename)
-        proc = subprocess.Popen(["jnml "+script_dir+"/integrationWindow.xml"],
-                                shell=True,
-                                stdout=subprocess.PIPE)
-        proc.communicate()
+        sim_data = simulation_tools.simulate_poisson_stimulation(exc_rate, sim_duration)
         
-        sim_data = np.loadtxt(out_filename)
         timepoints = sim_data[:,0]
         voltage = sim_data[:,1]
         spike_times = timepoints[np.diff(sim_data[:,2]) != 0]
-        if not spike_times.size:
-            print('no output spikes for input firing rate of {}Hz!'.format(exc_rate))
-            break
+        out_rates[k] = float(spike_times.size) / sim_duration
+        print("input: {}Hz; output: {}Hz".format(exc_rate, out_rates[k]))
 
         relative_times = np.zeros(shape=(spike_times.size, spike_times.size))
         for s, t in enumerate(spike_times):
             relative_times[s] = spike_times - t
-        #relative_times = (np.atleast_2d(spike_times) - np.atleast_2d(spike_times).transpose()).flatten()
         small_relative_times = relative_times[relative_times > 0]
         small_relative_times = small_relative_times[small_relative_times < upper_hist_limit]
         if small_relative_times.size:
@@ -69,7 +42,13 @@ def main():
             ax.set_title('Spike time autocorrelation for stim rate {}Hz'.format(exc_rate))
             ax.set_xlabel('lag (s)')
             ax.set_ylabel('relative frequency (spike probability density)')
-    plt.show()
+
+    if out_rates.size > 1:
+        ff_fig, ff_ax = plt.subplots()
+        ff_ax.plot(exc_rate_range, out_rates, color='k', linewidth=1.5)
+        ff_ax.set_xlabel("MF firing rate (Hz)")
+        ff_ax.set_xlabel("GrC firing rate (Hz)")
+        plt.show()
 
 if __name__ == "__main__":
     main()
---
+++ neuroConstruct/lemsSimulations/simulation_tools.py
@@ -0,0 +1,102 @@
+import os
+import subprocess
+import datetime
+import xml.dom.minidom
+import numpy as np
+import h5py
+import tempfile
+from os.path import dirname, abspath, join
+from xml.etree import ElementTree as ET
+from matplotlib import pyplot as plt
+
+
+def pretty_xml_string(element):
+    # sometimes jLEMS freaks out on unprettified xml. This is meant to
+    # work around that.
+    ugly_xml = xml.dom.minidom.parseString(ET.tostring(element))
+    return ugly_xml.toprettyxml()
+
+class StimulusLEMSAdapter(object):
+    def __init__(self, exc_rate):
+        self.lems = ET.Element("Lems")
+        exc_spikegen = ET.SubElement(self.lems, "spikeGeneratorRefPoisson")
+        exc_spikegen.set("id", "mossySpiker")
+        exc_spikegen.set("averageRate", "{} Hz".format(exc_rate))
+        exc_spikegen.set("minimumISI", "1 ms")
+
+class SimulationConfigurationLEMSAdapter(object):
+    def __init__(self, length, out_filename, step=0.05, target="poissonStimNetwork"):
+        self.lems = ET.Element("Lems")
+        simulation = ET.SubElement(self.lems, "Simulation")
+        simulation.set("id", "generatedSimulation")
+        simulation.set("length", "{} s".format(length))
+        simulation.set("step", "{} ms".format(step))
+        simulation.set("target", target)
+        output_file = ET.SubElement(simulation, "OutputFile")
+        output_file.set("id", "of0")
+        output_file.set("fileName", out_filename)
+        voltage_column = ET.SubElement(output_file, "OutputColumn")
+        voltage_column.set("id", "v")
+        voltage_column.set("quantity", "GrCPop[0]/v")
+        spike_column = ET.SubElement(output_file, "OutputColumn")
+        spike_column.set("id", "sc")
+        spike_column.set("quantity", "SpikeRecorderPop[0]/spikeRecorder/spikeCount")
+
+class CustomLEMSInclude(object):
+    def __init__(self, filename):
+        self.lems = ET.Element("Lems")
+        include_element = ET.SubElement(self.lems, "Include")
+        include_element.set("file", filename)
+        
+def simulate_poisson_stimulation(exc_rate, sim_duration_in_s):
+    # create file where simulation data will be stored
+    sim_data_file = tempfile.NamedTemporaryFile(delete=False, dir='./')
+    sim_data_file.close()
+
+    project_dir = dirname(dirname(abspath(__file__)))
+    # load base template for xml simulation description
+    template_filename = join(join(project_dir, "lemsSimulations"), "poisson_inputs_simulation_template.xml")
+    lems_tree = ET.parse(template_filename)
+    lems_root = lems_tree.getroot()
+
+    # define includes for custom component types and components
+    cell_mechs_dir = join(project_dir, 'cellMechanisms')
+    custom_lems_defs_dir = join(project_dir, "lemsDefinitions")
+    include_filenames = [join(custom_lems_defs_dir, 'spikeRecorder.xml'),
+                         join(custom_lems_defs_dir, 'spikeGeneratorRefPoisson.xml')]
+    for component_name in ['RothmanMFToGrCAMPA', 'RothmanMFToGrCNMDA', 'IaF_GrC']:
+        include_filenames.append(join(join(cell_mechs_dir, component_name), component_name+'.nml'))
+    lems_includes = [CustomLEMSInclude(filename).lems for filename in include_filenames]
+
+    # define procedurally generated lems elements
+    lems_stim = StimulusLEMSAdapter(exc_rate).lems
+    lems_sim = SimulationConfigurationLEMSAdapter(length=sim_duration_in_s,
+                                                  out_filename=sim_data_file.name).lems
+    
+    # insert custom includes in simulation description template
+    position_includes = 5
+    position_stim = 10
+    position_sim = 13
+    for k, inc in enumerate(lems_includes):
+        lems_root.insert(position_includes, inc[0])
+        position_includes += 1
+        position_stim += 1
+        position_sim += 1
+    # insert procedurally generated lems
+    lems_root.insert(position_stim, lems_stim[0])
+    lems_root.insert(position_sim, lems_sim[0])
+    # write simulation description to disk
+    sim_description_file = tempfile.NamedTemporaryFile(delete=False, dir='./')
+    sim_description_file.write(pretty_xml_string(lems_root))
+    sim_description_file.close()
+    # run jLEMS
+    proc = subprocess.Popen(["jnml {}".format(sim_description_file.name)],
+                            shell=True,
+                            stdout=subprocess.PIPE)
+    proc.communicate()
+    # remove simulation description file
+    os.remove(sim_description_file.name)
+    # read in output firing rate and remove simulation data file
+    out_data = np.loadtxt(sim_data_file.name)
+    os.remove(sim_data_file.name)
+    return out_data
