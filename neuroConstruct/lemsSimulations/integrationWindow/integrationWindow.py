c03ccab067a1bcabb241b0ecd4a555db0592e25a
Eugenio Piasini <e.piasini@ucl.ac.uk>
2014-02-28 18:27:18 +0000

    Update autocorrelation plot script to show correct asymptotic value.

--- neuroConstruct/lemsSimulations/integrationWindow/integrationWindow.py
+++ neuroConstruct/lemsSimulations/integrationWindow/integrationWindow.py
@@ -13,14 +13,15 @@ sys.path.append(os.path.dirname(script_dir))
 import simulation_tools
 
 def main():
-    exc_rate_range = np.array([50])
+    exc_rate_range = np.array([80])
     out_rates = np.zeros_like(exc_rate_range)
-    sim_duration = 60. # s
+    sim_duration = 400. # s
     n_bins = 200
-    upper_hist_limit = 0.300 # s
+    upper_hist_limit = 0.100 # s
 
     for k, exc_rate in enumerate(exc_rate_range):
-        sim_data = simulation_tools.simulate_poisson_stimulation(exc_rate, sim_duration)
+        exc_rates = [exc_rate, exc_rate, exc_rate, 10]
+        sim_data = simulation_tools.simulate_poisson_stimulation(exc_rates, sim_duration)
         
         timepoints = sim_data[:,0]
         voltage = sim_data[:,1]
@@ -28,27 +29,28 @@ def main():
         out_rates[k] = float(spike_times.size) / sim_duration
         print("input: {}Hz; output: {}Hz".format(exc_rate, out_rates[k]))
 
-        relative_times = np.zeros(shape=(spike_times.size, spike_times.size))
+        relative_times = []
         for s, t in enumerate(spike_times):
-            relative_times[s] = spike_times - t
-        small_relative_times = relative_times[relative_times > 0]
-        small_relative_times = small_relative_times[small_relative_times < upper_hist_limit]
+            stop_idx = np.searchsorted(spike_times, t+upper_hist_limit)
+            relative_times.extend((spike_times[s+1:stop_idx] - t).tolist())
+        small_relative_times = np.array(relative_times)
         if small_relative_times.size:
             fig, ax = plt.subplots()
-            n, bins, patches = ax.hist(small_relative_times, bins=n_bins, color='k', normed=True)
-            uniform_baseline = 1. / (bins[-1] - bins[0])
-
-            ax.plot([0, upper_hist_limit], [uniform_baseline, uniform_baseline], color='r', linewidth=1.5)
+            bin_length = upper_hist_limit/n_bins
+            weights = np.ones_like(small_relative_times)/(bin_length*sim_duration)
+            n, bins, patches = ax.hist(small_relative_times*1000, bins=n_bins, color='k', weights=weights, normed=False)
+            uniform_baseline_spikes_per_bin = out_rates[k]*bin_length
+            ax.plot([0, bins[-1]], [out_rates[k]**2, out_rates[k]**2], color='r', linewidth=1.5)
             ax.set_title('Spike time autocorrelation for stim rate {}Hz'.format(exc_rate))
-            ax.set_xlabel('lag (s)')
-            ax.set_ylabel('relative frequency (spike probability density)')
+            ax.set_xlabel('lag (ms)')
+            ax.set_ylabel('autocorrelation (Hz$^2$)')
 
     if out_rates.size > 1:
         ff_fig, ff_ax = plt.subplots()
         ff_ax.plot(exc_rate_range, out_rates, color='k', linewidth=1.5)
         ff_ax.set_xlabel("MF firing rate (Hz)")
         ff_ax.set_xlabel("GrC firing rate (Hz)")
-        plt.show()
+    plt.show()
 
 if __name__ == "__main__":
     main()
